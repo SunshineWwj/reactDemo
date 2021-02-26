4903d8f27b12b3786c976c3cc50bb0b04d2412fe
wangweijuan <wangweijuan@sdscm.com>
2021-02-26 13:34:55 +0800

    自定义hooks

--- src/useHooksToCount/count.js
+++ src/useHooksToCount/count.js
@@ -1,13 +1,53 @@
+/* eslint-disable no-undef */
+/* eslint-disable func-style */
+/* eslint-disable no-unused-vars */
 /* eslint-disable no-plusplus */
-import React, {useReducer} from 'react';
+import React, {useState, useEffect, useReducer} from 'react';
 import {defaultState, CounterReducer} from './hooksReducer';
 import SubComponent from './subComponent';
 import {CounterContext} from './context';
 
+// function useWindowWidth() {
+//     const [width, setWidth] = useState(window.innerWidth);
+//     useEffect(() => {
+//         const handleResize = () => {
+//             setWidth(window.innerWidth);
+//         };
+//         window.addEventListener('resize', handleResize);
+//         return () => {
+//             //取消监听事件
+//             window.removeEventListener('resize', handleResize);
+//         };
+//     }, [width]);
+
+//     return width;
+// }
+// export default function windowWidth() {
+//     const width = useWindowWidth();
+//     return (
+//         <div>window width is {width}</div>
+//     );
+// }
+
+
+// export default function RenderFunctionComponent() {
+//     const [firstName, setFirstName] = useState('Rudi');
+//     const [lastName, setLastName] = useState('Yardley');
+  
+//     return (
+//         <React.Fragment>
+//             <button onClick={() => setFirstName('Fred')}>{firstName}</button><br/>
+//             <button onClick={() => setLastName('David')}>{lastName}</button>
+//         </React.Fragment>
+//     );
+// }
+
+
 let iCount = 0;
 export default function Counter() {
     const [state, dispatch] = useReducer(CounterReducer, defaultState);
-
+    const countReducer = useReducer(CounterReducer, defaultState);
+    console.log('countReducer:', countReducer);
     return (
         <CounterContext.Provider value={{state,
             dispatch}}>
@@ -30,6 +70,8 @@ export default function Counter() {
                 <hr/>
 
                 <SubComponent></SubComponent>
+
+
             </div>
         </CounterContext.Provider>
     );
--- src/useHooksToCount/subComponent/index.js
+++ src/useHooksToCount/subComponent/index.js
@@ -4,6 +4,7 @@ import {CounterContext} from '../context';
 
 export default function SubComponent() {
     const counterContext = useContext(CounterContext);
+    console.log('counterContext:', counterContext);
     return (
         <div>子组件计数器：{counterContext.state.count}</div>
     );
